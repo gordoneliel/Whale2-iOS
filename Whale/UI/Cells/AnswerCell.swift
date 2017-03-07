e88a75698058189d5a03aa93b3ec9feb8d1ac4c5
Eliel Gordon <gordoneliel@gmail.com>
2017-03-06 23:57:36 -0800

    Adds storyboard scaffold and answer cell

---
+++ Whale/UI/Cells/AnswerCell.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerCell.swift
+//  Whale
+//
+//  Created by Eliel A. Gordon on 3/6/17.
+//  Copyright © 2017 Eliel A. Gordon. All rights reserved.
+//
+
+import UIKit
+
+class AnswerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
